96f462d75e85311f18927b1cc0eb1b3a25a85a77
Kenneth Okalang <okalang.ok@gmail.com>
2023-08-09 20:09:12 +0200

    Add project files.

---
+++ KompInvest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KompInvest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
