a0ada9b83b2068e5978ddf76e4388ecc62729698
Kenneth Okalang <okalang.ok@gmail.com>
2023-11-24 16:32:11 +0100

    Program

--- KompInvest/Program.cs
+++ KompInvest/Program.cs
@@ -1,28 +1,28 @@
+using KompInvest;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System;
 
-namespace KompInvest
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>();
-            webBuilder.ConfigureKestrel(serverOptions =>
-            {
-                // Bind to the port provided by Heroku
-                int port = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "5000");
-                serverOptions.ListenAnyIP(port);
-            });
-        });
-
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureKestrel(serverOptions =>
+                {
+                    // Heroku dynamically assigns a port
+                    var port = Environment.GetEnvironmentVariable("PORT");
+                    if (!string.IsNullOrEmpty(port))
+                    {
+                        serverOptions.ListenAnyIP(int.Parse(port));
+                    }
+                });
+            });
 }
